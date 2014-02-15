e1104007b29ef81909495ddf6ad9e4892582f0bc
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-02-15 13:19:40 -0500

    Refine collision and add score
    - Add score
    - Add new "passage" to allow increase in score
    - Modify barrier construction logic
    - Add end game when bee collides with barrier

--- js/game.js
+++ js/game.js
@@ -23,46 +23,60 @@ var Bee = {
   }
 
 };
+
+var BARRIER_WIDTH = 60;
+var BARRIER_FREQUENCY = 200;
+
 var barriers;
+var passages;
+var score;
+var scoreDisplay;
+var song;
 
 function preload() {
   game.load.image('bee', 'assets/bumblebee.png');
-  game.load.image('wall', 'assets/bumblebee_wall.png');
+  game.load.image('wall', 'assets/bumblebee_spider.png');
+  game.load.image('passage', 'assets/bumblebee_passage.png');
   game.load.audio('song', 'assets/fobb.mp3');
 }
 
 function create() {
   game.world.setBounds(0, 0, World.WIDTH, World.HEIGHT);
+  game.stage.backgroundColor = "20DEFF";
 
   bee = game.add.sprite(60, game.world.centerY, 'bee');
   bee.anchor.setTo(0.5, 0.5);
   bee.scale.setTo(0.5, 0.5);
+
 
   game.camera.follow(bee);
 
   song = game.add.audio('song');
   song.play();
 
-  // barrier props
-  var barrierConfig = {
-    width: 60,
-    height: 200,
-    variance: 100
-  };
   // group to hold barriers
   barriers = game.add.group();
+  passages = game.add.group();
+
   //generage barriers
-  for(var i = barrierConfig.width; i < World.WIDTH; i+= (barrierConfig.width * 1.5 + generateVariance(barrierConfig.width))) {
-    //create bottom barrier
-    createBarrier(barrierConfig, i, true);
-    //create corresponding top barrier
-    createBarrier(barrierConfig, i, false);
+  for(var i = Win.WIDTH / 3; i < World.WIDTH; i+= BARRIER_FREQUENCY) {
+    passageHeight = Math.random() * 150 + 50;
+    passageY = Math.random() * (Win.HEIGHT - passageHeight - 200) + 100;
+
+    createBarrier(i, passageY, passageHeight, true)
+    createPassage(i, passageY, passageHeight);
+    createBarrier(i, passageY, passageHeight, false)
   }
 
+  score = 0;
+  var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
+  scoreDisplay = game.add.text(Win.WIDTH / 2, 0, score.toString(), style);  
+  scoreDisplay.fixedToCamera = true;
 }
 
 function update() {
-  game.physics.collide(bee, barriers);
+  game.physics.collide(bee, barriers, deathCollision);
+  game.physics.overlap(bee, passages, increaseScore);
 
   if(game.input.mousePointer.isDown) {
     expandBee();
@@ -78,6 +92,17 @@ function update() {
 }
 
 function render() {
+}
+
+function deathCollision(bee, barrier) {
+  bee.kill();
+  song.stop();
+}
+
+function increaseScore(bee, passage) {
+  score = score + 1;
+  scoreDisplay.content = score.toString();
+  passage.kill();
 }
 
 function shrinkBee() {
@@ -98,21 +123,28 @@ function generateVariance(val) {
   return val * Math.random();
 }
 
-function createBarrier(barrierConfig, xCoord, isBottom) {
-
-  var randomHeight = barrierConfig.height + generateVariance(barrierConfig.variance);
-  var yCoord = 0;
-  if(isBottom) {
-    yCoord = Win.HEIGHT - randomHeight;
-  }
-
-//  wall.drawRect(xCoord, yCoord, barrierConfig.width, randomHeight);
+function createBarrier(x, y, passageHeight, isBottom) {
 
   var barrier = game.add.sprite(0, 0, 'wall');
-  barrier.x = xCoord;
-  barrier.y = yCoord;
-  barrier.height = randomHeight;
-  barrier.width = barrierConfig.width;
+  barrier.x = x;
+  if (isBottom) {
+    barrier.y = y + passageHeight;
+    barrier.height = Win.HEIGHT - (passageHeight + y);
+  } else {
+    barrier.y = 0;
+    barrier.height = y;
+  }
+  barrier.width = BARRIER_WIDTH;
   barrier.body.immovable = true;
   barriers.add(barrier);
+}
+
+function createPassage(x, y, height) {
+  var passage = game.add.sprite(0.5, 0.5, 'passage');
+  passage.x = x;
+  passage.y = y;
+  passage.height = height;
+  passage.width = 60;
+  passage.body.immovable = true;
+  passages.add(passage);    
 }
