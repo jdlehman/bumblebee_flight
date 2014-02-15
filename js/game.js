23a7e18cc629e35daeebed16df54e229a14eae0d
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-02-14 21:14:32 -0500

    Add bee
    - Add bee player as a sprite
    - Shrink/expand player if the up arrow key is pressed/release

--- js/game.js
+++ js/game.js
@@ -1,10 +1,39 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var bee;
+var upKey;
+var beeSize = 1;
+
 function preload() {
+  game.load.image('bee', 'assets/bumblebee.png');
 }
 
 function create() {
+  bee = game.add.sprite(game.world.centerX, game.world.centerY, 'bee');
+  bee.anchor.setTo(0.5, 0.5);
+
+  upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
 }
 
 function update() {
+  if (upKey.isDown) {
+    shrinkBee();
+  } else {
+    expandBee();
+  }
 }
+
+function shrinkBee() {
+  if (beeSize > .2) {
+    beeSize = beeSize - .1;
+    bee.scale.setTo(beeSize, beeSize);
+  }
+}
+
+function expandBee() {
+  if (beeSize < 1) {
+    beeSize = beeSize + .1;
+    bee.scale.setTo(beeSize, beeSize);
+  }
+}
+
